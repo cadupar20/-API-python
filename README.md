98bd370b872bf90b33b010c35f125aac5bd0beff
Ariel Dupar <adupar@gmail.com>
2024-10-16 10:42:13 -0300

    actualizado readme.md, main.py con mas detalles + endpoints, nuevo requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # API-python
+
+### Requisitos: 
+
+pip install -r requirements.txt
+(fastapi uvicorn etc)
+
+# Cear el virtual environment:
+❯ virtualenv venv
+
+# Activar el virtual environment:
+❯ .\venv\Scripts\activate
+
+# Run the First API App With Uvicorn
+### Iniciar el servidor: uvicorn main:app --reload
+
+### Documentación de la API: 
+Consulte la documentación de la API interactiva
+Ahora abra http://127.0.0.1:8000/docs en su navegador.
+
+Verá la documentación API interactiva automática proporcionada por Swagger UI:
+![Swagger UI](https://files.realpython.com/media/fastapi-first-steps-01-swagger-ui-simple.c46a4a9242dd.png)
+
+### Pruebe la documentación de la API:
+
+Tambien puede probar la URL de documentación alternativa de la API. Dirígete a http://127.0.0.1:8000/redoc.
+Aquí verás la documentación automática alternativa (proveída por ReDoc):
+![ReDoc](https://fastapi.tiangolo.com/img/index/index-02-redoc-simple.png)
+
+
+Dentro de la documentación de este main.py, se puede probar los siguientes endpoints: 
+
+
+| URL    | Metodo |  Body  |
+| ------ | ------ | ------ |
+| http://127.0.0.1:8000/api/v1/hello | GET | NO |
+| http://127.0.0.1:8000/api/v1/items/10 | GET | NO |
+| http://127.0.0.1:8000/api/v1/items/foo | GET | NO |
+| http://127.0.0.1:8000/api/v1/add | PUT | NO |
+| http://127.0.0.1:8000/api/v1/put?newtext=_2do%20String_%203er%20String | PUT | NO |
+| http://127.0.0.1:8000/api/v1/change?text=_2do%20String_%203er%20String | POST | NO |
+| http://127.0.0.1:8000/api/v1/delete | PUT | NO |
+| http://127.0.0.1:8000/api/v2/items/ | POST | SI |
+| http://127.0.0.1:8000/api/v3/items/10 | POST | SI |
+
+con los metodos GET, POST, PUT y DELETE que se han desarrollado como ejemplo dentro del este archivo main.py (ejemplo: Este endopoint utiliza el metodo PUT --> @app.put("/api/v1/delete"))
+
+Es importante para algunos de las URLs que se han creado, ademas de los parametros es necesario que el request tengo un Body tipo JSON como el siguiente ejemplo:
+
+### Esto se puede validar utilizando POSTMAN tambien, en caso de no usar /docs o /Redoc, se puede probar con las herramientas API Rest:
+http://127.0.0.1:8000/api/v2/items/
+
+Body Raw JSON Example:
+
+{
+  "name": "Sony TV 54 P32TV74R2",
+  "description": "TV 354 Pulgadas Sony modelo P32TV74R2 - Full HD - 3 HDMI Ports",
+  "price": 675000,
+  "tax": 10.3
+}
+
+![Postman image1](https://i.imgur.com/HRFD7o6.jpeg)
+
+### Esto se puede validar utilizando POSTMAN tambien, en caso de no usar /docs o /Redoc, se puede probar con las herramientas API Rest:
+http://127.0.0.1:8000/api/v3/items/10
+
+Body Raw JSON Example:
+
+{
+  "name": "Sony TV 32 P32TV54R2",
+  "description": "TV 32 Pulgadas Sony modelo P32TV54R2 - Full HD - 3 HDMI Ports",
+  "price": 345000,
+  "tax": 10.3
+}
+
+![Postman image2](https://i.imgur.com/QRvfxlO.jpeg)
+
+
+### REFERENCE: 
+1) Documentación del uso de FastAPI -> https://realpython.com/fastapi-python-web-apis/
+2) Sitio para publicar imagenes -> https://imgur.com
+3) FastAPI Documentation -> https://fastapi.tiangolo.com/es/tutorial/first-steps/#paso-4-define-la-funcion-de-la-operacion-de-path
--- main.py
+++ main.py
@@ -1,43 +1,107 @@
 # uvicorn main:app
 
 from typing import Union 
-from fastapi import FastAPI #pip install fastapi, uvicorn
+from fastapi import FastAPI, Depends #pip install fastapi, uvicorn
+from typing import Optional, List
+from pydantic import BaseModel
 
+# Se puede ver la documentación en http://127.0.0.1:8000/docs de FastAPI de manera sencilla
 app = FastAPI()
 
-
-
+# Funcion GET, devuelve el texto "Hello World"
+# http://127.0.0.1:8000/api/v1/hello (el dato es devuelto en formato JSON)
 @app.get("/api/v1/hello")
 async def principal():
     return {"Hello": "World"}
 
-
+# Funcion GET para leer un item_id (numero entero) y un string Q pasado como parametro, devuelve el item_id numerico y el string q
+# http://127.0.0.1:8000/items/{item_id}?q=foo (el dato es devuelto en formato JSON)
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# Funcion GET para leer un item_id que se lo pasa como parametro, devuelve el item_id la función
+# http://127.0.0.1:8000/api/v1/items/{item_id} // item_id = 10 o item_id = foo
+@app.get("/api/v1/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
 
-
+# Variable GLOBAL usada para almacenar el texto
 text_message = "Initial Text Message"
 
-# Funcion POST para agregar un String dentro de la variable text_message existente
+# Funcion POST para agregar (ADD) un String dentro de la variable text_message existente
+# http://127.0.0.1:8000/api/v1/change?text=_2do%20String_%203er%20String (POST
 @app.post("/api/v1/add")
 async def add_text(text: str):
+    global text_message
+    old_text = text_message
     text_message += text
-    return {"message": "Text Added", "current_message": text_message}
+    return {"message": "Text Added", "current_message": text_message, "old_text": old_text}
 
-# Funcion PUT para cambiar un String dentro de la variable text_message existente
+
+# Funcion PUT para CAMBIAR un String dentro de la variable text_message existente
+# http://127.0.0.1:8000/api/v1/put?newtext=_2do%20String_%203er%20String (PUT)
 @app.put("/api/v1/put")
 async def change_text(newtext: str):
-    text_message += newtext
-    return {"message": "Text Changed", "current_message": text_message}
+    global text_message
+    old_text = text_message
+    text_message = newtext
+    return {"message": "Text Changed", "current_message": text_message, "old_text": old_text}
 
 
 # Funcion DELETE para borrar text_message
-@app.put("/api/v1/put")
+# http://127.0.0.1:8000/api/v1/delete (PUT)
+@app.put("/api/v1/delete")
 async def delete_text():
+    global text_message
+    old_text = text_message
     text_message = ""
-    return {"message": "Text Removed", "current_message": text_message}
+    return {"message": "Text Removed", "current_message": text_message, "old_text": old_text}
+
+
+global Item
+
+# A Pydantic Item object
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+# Función POST para agregar un Item a una lista existente, devuelve el item.dict()
+# http://127.0.0.1:8000/api/v2/items/
+# Body Raw JSON Example: { "name": "Sony TV 32 P32TV54R2",   "description": "TV 32 Pulgadas Sony modelo P32TV54R2 - Full HD - 3 HDMI Ports",   "price": 345000,   "tax": 10.3 }
+@app.post("/api/v2/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price * (1+ (item.tax/100))
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# Función POST para agregar un Item a una lista existente, devuelve el item_id y el item.dict()
+# http://127.0.0.1:8000/api/v3/items/10
+# Body Raw JSON Example: { "name": "Sony TV 32 P32TV54R2",   "description": "TV 32 Pulgadas Sony modelo P32TV54R2 - Full HD - 3 HDMI Ports",   "price": 345000,   "tax": 10.3 }
+@app.post("/api/v3/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+# A Pydantic Place object
+class Place(BaseModel):
+    name: str
+    description: Optional[str] = None
+    coffee: bool
+    wifi: bool
+    food: bool
+    lat: float
+    lng: float
+
+    class Config:
+        orm_mode = True
+
+# Función POST para agregar un Place a una lista existente, devuelve el place
+# http://127.0.0.1:8000/api/v1/places-basic-example/
+# Body Raw JSON Example: { "name": "Café de la Piscine",   "description": "Café de la Piscine",   "coffee": true,   "wifi": true,   "food": true,   "lat": 48.8566,   "lng": 2.3522 }
+@app.post('/api/v1/places-basic-example/')
+async def create_place_view(place: Place):
+    return place
